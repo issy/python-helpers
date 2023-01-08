727124f8306b13e0ad51d4c2d447d0d8def98ce2
issy <issy@issy.dev>
2023-01-08 20:17:10 +0000

    Add project files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="helpers",
+    description="Functional programming helpers",
+    version="0.0.1",
+    url="https://github.com/issy/python-helpers",
+    license="MIT",
+    packages=["src"],
+    python_requires=">=3.9.0",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+)
